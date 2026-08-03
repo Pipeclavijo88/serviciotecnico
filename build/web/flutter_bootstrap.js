3b58b490af13ee69232f7e36011584c75e6628c3
Pipeclavijo88 <afcmontoya@gmail.com>
2026-08-03 08:28:50 -0500

    Actualizacion de app

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1932453250" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2932590694" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
